3528084002ba69e1736eadae6886a1a926354052
Harsika Singh <117065981+harsikaSingh@users.noreply.github.com>
2023-02-26 01:26:57 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { BrowserRouter, BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import "./App.css";
+import Cart from './Components/Cart/Cart';
+import FullData from './Components/FullData/FullData';
+import Home from './Components/Home/Home';
+import Buy from './Components/BuyNow/Buy'
+
+
+
+const App = () => {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='product/:id' element={<FullData />} />
+          <Route path='/cart' element={<Cart />} />
+          <Route path='product/:id/cart' element={<Cart />} />
+          <Route path='/buyNow' element={<Buy />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App;
